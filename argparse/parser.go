264fff922e8d55c8f17b25491f09b252d4a37e95
jx3yang <65411020+jx3yang@users.noreply.github.com>
2021-09-15 00:34:02 -0400

    add arguments parsing

--- argparse/parser.go
+++ argparse/parser.go
@@ -18,22 +18,23 @@ type Arguments struct {
 
 func Parse() (*Arguments, error) {
 	args := &Arguments{}
-	if len(os.Args) < 2 {
-		return nil, errors.New("usage: latexsplit <filename> [-c=pdflatex] [-args=\"-jobname=<filestem>\"] [-split=\"% latexsplit\"]")
+	flag.StringVar(&args.FileName, "file", "", "TeX file to compile")
+	flag.StringVar(&args.Compiler, "c", "pdflatex", "Compiler")
+	flag.StringVar(&args.CompilerArgs, "args", "", "Arguments to the compiler")
+	flag.StringVar(&args.SplitLine, "split", "% latexsplit", "Line to split")
+
+	flag.Parse()
+
+	if args.FileName == "" {
+		return nil, errors.New("usage: latexsplit -file <filename> [-c pdflatex] [-args \"-jobname=<filestem>\"] [-split \"% latexsplit\"]")
 	}
 
-	args.FileName = os.Args[1]
 	_, err := os.Stat(args.FileName)
 	if os.IsNotExist(err) {
 		return nil, errors.New("Could not find file " + args.FileName)
 	}
 	args.FileStem = filepath.Base(args.FileName)[0 : len(args.FileName)-len(filepath.Ext(args.FileName))]
 
-	flag.StringVar(&args.Compiler, "Compiler name", "pdflatex", "")
-	flag.StringVar(&args.CompilerArgs, "Compiler arguments", "", "")
-	flag.StringVar(&args.SplitLine, "Line to split", "% latexsplit", "")
-
-	flag.Parse()
 	if len(args.CompilerArgs) > 0 {
 		args.CompilerArgs = fmt.Sprintf("-jobname=%v %v", args.FileStem, args.CompilerArgs)
 	} else {
